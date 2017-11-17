83d3e248f8a7d05936eab45f41a96c722e54ef13
stories2 <stories2@naver.com>
2017-11-18 02:14:58 +0900

    HelloWorld module : catch event handler

--- src/main/java/com/stories2/mod/HelloWorld.java
+++ src/main/java/com/stories2/mod/HelloWorld.java
@@ -3,8 +3,12 @@ package com.stories2.mod;
 import com.stories2.mod.proxy.CommonProxy;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION, acceptedMinecraftVersions =  Reference.ACCEPTED_VERSIONS)
 public class HelloWorld {
@@ -14,4 +18,19 @@ public class HelloWorld {
 
 	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
 	public static CommonProxy proxy; 
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent initEvent) {
+		
+	}
+	
+	@EventHandler
+	public void Init(FMLInitializationEvent initEvent) {
+		
+	}
+	
+	@EventHandler
+	public void PostInit(FMLPostInitializationEvent initEvent) {
+		
+	}
 }
