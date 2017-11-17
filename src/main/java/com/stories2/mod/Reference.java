e59fda24dd51de8d989f59eec827824acb3e22a8
stories2 <stories2@naver.com>
2017-11-18 02:30:04 +0900

    info : change mod id

--- src/main/java/com/stories2/mod/Reference.java
+++ src/main/java/com/stories2/mod/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
 	public static final String
 //		com.stories2.mod
-		MOD_ID = "stories2 mod",
+		MOD_ID = "sm",
 		NAME = "Tutorial of create minecraft mod",
 		VERSION = "0.0.1",
 		ACCEPTED_VERSIONS =  "[1.12.2]",
