f39124cfdd192ce044fc067ed9aef93ec64c847e
stories2 <stories2@naver.com>
2017-11-18 02:38:25 +0900

    ModItems module : init mod items class

---
+++ src/main/java/init/ModItems.java
@@ -0,0 +1,22 @@
+package init;
+
+import net.minecraft.item.Item;
+
+public class ModItems {
+
+	public static void init() {
+		
+	}
+	
+	public static void register() {
+		
+	}
+	
+	public static void registerRenders() {
+		 
+	}
+	
+	private static void registerRender(Item item) {
+		
+	}
+}
