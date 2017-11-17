1b973c82800a8139fe4d9927f8d5c65234d41d31
stories2 <stories2@naver.com>
2017-11-18 01:52:10 +0900

    HelloWorld module : make new helloworld class instance

--- src/main/java/com/stories2/mod/HelloWorld.java
+++ src/main/java/com/stories2/mod/HelloWorld.java
@@ -1,5 +1,12 @@
 package com.stories2.mod;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.Instance;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION, acceptedMinecraftVersions =  Reference.ACCEPTED_VERSIONS)
 public class HelloWorld {
+
+	@Instance
+	public static HelloWorld instance;
 
 }
